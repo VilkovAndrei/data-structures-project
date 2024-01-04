22254e0969162079ffe38409381180b584b24e77
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-01-04 20:14:58 +0300

    HW1 and HW2

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,16 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+
+    def test_Stack(self):
+        stack1 = Stack()
+        stack1.push('data1')
+        stack1.push('data2')
+        stack1.push('data3')
+        self.assertEqual(stack1.top.data, 'data3')
+        self.assertEqual(stack1.top.next_node.data, 'data2')
+        self.assertEqual(stack1.top.next_node.next_node.data, 'data1')
+        stack1.pop()
+        self.assertEqual(stack1.pop(), 'data2')
