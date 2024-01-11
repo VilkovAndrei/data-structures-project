0e7cdedda205fa81b1fe9c84b9cd4accdb06b6bc
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-01-11 22:09:53 +0300

    HW3

--- src/queue.py
+++ src/queue.py
@@ -16,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.all = []
+        self.head = None
 
     def enqueue(self, data):
         """
@@ -24,32 +25,41 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        node = Node(data, None)
+        new_node = Node(data, None)
         if self.head is None:
-            self.head = node
-            self.tail = node
+            self.head = new_node
+            self.tail = self.head
             self.all.append(data)
         else:
-            self.tail.next_node = node
-            self.tail = node
+            self.tail.next_node = new_node
+            self.tail = new_node
             self.all.append(data)
-        def enqueue(self, data):
-            new_data = Node(data)
-            if self.head is None:
-                self.head = new_data
-                self.tail = self.head
-            else:
-                self.tail.next = new_data
-                self.tail = new_data
 
     def dequeue(self):
         """
-        Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
+        Метод для удаления элемента из головы очереди. Возвращает данные удаленного элемента
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.head is None:
+            return None
+        else:
+            popped_node = self.head
+            self.head = self.head.next_node
+            return popped_node.data
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+
+        result_list = []
+        if self.head == None:
+            return ""
+        else:
+            result_list.append(self.head.data)
+            current_node = self.head.next_node
+            while current_node != None:
+                result_list.append(current_node.data)
+                current_node = current_node.next_node
+
+            result = "\n".join(result_list)
+            return result
