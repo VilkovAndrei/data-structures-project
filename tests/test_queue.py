4d394499bbcd6db4b0055d05e77bbbf9fe21ec2b
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-01-12 21:35:37 +0300

    HW3 edit queue.py add del_node_queue()

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,16 +1,19 @@
 import unittest
 from src.queue import Queue
 
+
 class TestQueue(unittest.TestCase):
     def test_Queue(self):
         queue1 = Queue()
         queue1.enqueue('data1')
         queue1.enqueue('data2')
         queue1.enqueue('data3')
-        self.assertEqual(str(queue1), 'data1\ndata2\ndata3')
+        queue1.enqueue('data4')
+        self.assertEqual(str(queue1), 'data1\ndata2\ndata3\ndata4')
         self.assertEqual(queue1.head.data, 'data1')
-        self.assertEqual(queue1.tail.data, 'data3')
+        self.assertEqual(queue1.tail.data, 'data4')
         self.assertEqual(queue1.head.next_node.data, 'data2')
         self.assertEqual(queue1.head.next_node.next_node.data, 'data3')
-        queue1.dequeue()
-        self.assertEqual(queue1.dequeue(), 'data2')
+        self.assertEqual(queue1.dequeue(), 'data1')
+        self.assertEqual(queue1.del_node_queue(1), 'data3')
+
