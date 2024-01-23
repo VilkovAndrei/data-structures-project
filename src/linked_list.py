b168bae9d99dc542a90851eed3182f4edd04e6a9
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-01-24 00:05:18 +0300

    HW6

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,5 +1,6 @@
 class Node:
     """Класс для узла односвязного списка"""
+
     def __init__(self, data: dict, next_node):
         """
         Конструктор класса Node
@@ -20,8 +21,9 @@ class LinkedList:
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными
-         в начало связанного списка
+        в начало связанного списка
         """
+
         new_node = Node(data, None)
         if self.head is None:
             self.head = new_node
@@ -43,6 +45,7 @@ class LinkedList:
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
+
         node = self.head
         if node is None:
             return str(None)
@@ -54,3 +57,38 @@ class LinkedList:
 
         ll_string += 'None'
         return ll_string
+
+    def to_list(self):
+        """Метод возвращает список с данными,
+        содержащимися в односвязном списке"""
+
+        result_list = []
+        if not self.head:
+            return []
+        else:
+            result_list.append(self.head.data)
+            current_node = self.head.next_node
+            while current_node:
+                result_list.append(current_node.data)
+                current_node = current_node.next_node
+            return result_list
+
+    def get_data_by_id(self, v_id: int):
+        """ Метод возвращает первый найденный в односвязном списке
+            словарь с ключом 'id'
+            или пустой список, если ключ не найден
+        """
+        ll_list = self.to_list()
+
+        for next_dict in ll_list:
+            try:
+                value = next_dict['id']
+
+            except TypeError:
+                print('Данные не являются словарем или в словаре нет id.')
+                continue
+            else:
+                if value == v_id:
+                    return next_dict
+                continue
+        return {}
